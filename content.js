658bc7eba64994df8c0d8a94ecbd6879cd28b3f8
jorsema2 <jorsema2@gmail.com>
2024-10-17 16:55:08 +0200

    Refactored to use MutationObserver

--- content.js
+++ content.js
@@ -2,7 +2,19 @@ function clickNextEpisode() {
     const nextButton = document.querySelector('[data-uia="next-episode-seamless-button"]');
     if (nextButton) {
         nextButton.click();
+        console.log('Clicked Next Episode button');
     }
 }
 
-setInterval(clickNextEpisode, 1000);
+const observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+        if (mutation.type === 'childList') {
+            clickNextEpisode();
+        }
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
